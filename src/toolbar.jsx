917b81a4beb5549a7b19554b0c641c8a545e77da
Kiho Chang <kihoc22@gmail.com>
2022-10-09 04:33:25 -0700

    Implement ToolbarGroupItem

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -5,12 +5,15 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import ToolbarItem from './toolbar-draggable-item';
+import ToolbarGroupItem from './toolbar-group-item';
+
 import ID from './UUID';
 import store from './stores/store';
+import { groupBy } from './functions';
 
 function isDefaultItem(item) {
   const keys = Object.keys(item);
-  return keys.filter(x => x !== 'element' && x !== 'key').length === 0;
+  return keys.filter(x => x !== 'element' && x !== 'key' && x !== 'group_name').length === 0;
 }
 
 function buildItems(items, defaultItems) {
@@ -21,9 +24,21 @@ function buildItems(items, defaultItems) {
     let found;
     if (isDefaultItem(x)) {
       found = defaultItems.find(y => (x.element || x.key) === (y.element || y.key));
+      if (x.group_name) {
+        found.group_name = x.group_name;
+      }
     }
     return found || x;
   });
+}
+
+function buildGroupItems(allItems) {
+  const items = allItems.filter(x => !x.group_name);
+  const gItems = allItems.filter(x => !!x.group_name);
+  const grouped = groupBy(gItems, x => x.group_name);
+  const groupKeys = gItems.map(x => x.group_name)
+    .filter((v, i, self) => self.indexOf(v) === i);
+  return { items, grouped, groupKeys };
 }
 
 class Toolbar extends React.Component {
@@ -319,6 +334,7 @@ class Toolbar extends React.Component {
       id: ID.uuid(),
       element: item.element || item.key,
       text: item.name,
+      group_name: item.group_name,
       static: item.static,
       required: false,
       showDescription: item.showDescription,
@@ -419,13 +435,19 @@ class Toolbar extends React.Component {
     store.dispatch('create', this.create(item));
   }
 
+  renderItem = (item) => (<ToolbarItem data={item} key={item.key} onClick={this._onClick.bind(this, item)} onCreate={this.create} />)
+
   render() {
+    const { items, grouped, groupKeys } = buildGroupItems(this.state.items);
     return (
       <div className="col-md-3 react-form-builder-toolbar float-right">
         <h4>{this.props.intl.formatMessage({ id: 'toolbox' })}</h4>
         <ul>
           {
-            this.state.items.map((item) => (<ToolbarItem data={item} key={item.key} onClick={this._onClick.bind(this, item)} onCreate={this.create} />))
+            items.map(this.renderItem)
+          }
+          {
+            groupKeys.map(k => <ToolbarGroupItem key={k} name={k} group={grouped.get(k)} renderItem={this.renderItem} />)
           }
         </ul>
       </div>
